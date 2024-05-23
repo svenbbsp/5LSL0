8893ac7c38d0a433547cc7c96f3deffaa31523a7
Dhani-AI <d.r.m.crapels@student.tue.nl>
2024-05-23 13:03:59 +0200

    Add model architecture

--- Assignment_3/autoencoder_template.py
+++ Assignment_3/autoencoder_template.py
@@ -1,28 +1,41 @@
 # %% imports
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 # %%  Encoder
 class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
-        # create layers here
-        
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, padding=1)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=1, kernel_size=3, padding=1)
+        self.pool2 = nn.AdaptiveMaxPool2d((2, 1))
+
     def forward(self, x):
-        # use the created layers here
-        h = x
-        return h
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool2(x)
+        return x
     
 # %%  Decoder
 class Decoder(nn.Module):
     def __init__(self):
         super(Decoder, self).__init__()
-        # create layers here
-        
-    def forward(self, h):
-        # use the created layers here
-        r = h
-        return r
+        self.up1 = nn.Upsample(scale_factor=(8, 16), mode='nearest')
+        self.convT1 = nn.ConvTranspose2d(in_channels=1, out_channels=16, kernel_size=3, padding=1)
+        self.up2 = nn.Upsample(scale_factor=2, mode='nearest')
+        self.convT2 = nn.ConvTranspose2d(in_channels=16, out_channels=1, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        x = self.up1(x)
+        x = F.relu(self.convT1(x))
+        x = self.up2(x)
+        x = F.relu(self.convT2(x))
+        return x
     
 # %%  Autoencoder
 class AE(nn.Module):
